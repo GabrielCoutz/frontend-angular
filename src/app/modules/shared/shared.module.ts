7a68d03a31344aa9ca3a808e7ec9a7ddf9e61b5a
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-02 08:10:49 -0300

    refactor(form.component): forms use class 'form' instead import 'form.component'

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProdutsListComponent } from './components/produts-list/produts-list.component';
-import { FormComponent } from './components/form/form.component';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-	declarations: [ProdutsListComponent, FormComponent],
+	declarations: [ProdutsListComponent],
 	imports: [CommonModule, FlexLayoutModule],
-	exports: [ProdutsListComponent, FormComponent],
+	exports: [ProdutsListComponent],
 })
 export class SharedModule {}
