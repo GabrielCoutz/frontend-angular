f5097915c11db362bdfbc591872c435d5ffab48c
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-02 08:48:20 -0300

    feat(header.component): add routes in links

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,35 +1,32 @@
 import {
-  BreakpointObserver,
-  Breakpoints,
-  BreakpointState,
+	BreakpointObserver,
+	Breakpoints,
+	BreakpointState,
 } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  constructor(private readonly breakpointObserver: BreakpointObserver) {}
-  destroyed = new Subject<void>();
-  mobileView = false;
+	constructor(private readonly breakpointObserver: BreakpointObserver) {}
+	destroyed = new Subject<void>();
+	mobileView = false;
 
-  ngOnInit() {
-    this.breakpointObserver
-      .observe([Breakpoints.XSmall])
-      .pipe(takeUntil(this.destroyed))
-      .subscribe({
-        next: (result: BreakpointState) => {
-          console.log(result);
-          this.mobileView = result.matches;
-        },
-      });
-  }
+	ngOnInit() {
+		this.breakpointObserver
+			.observe([Breakpoints.XSmall])
+			.pipe(takeUntil(this.destroyed))
+			.subscribe({
+				next: (result: BreakpointState) => (this.mobileView = result.matches),
+			});
+	}
 
-  ngOnDestroy() {
-    this.destroyed.next();
-    this.destroyed.complete();
-  }
+	ngOnDestroy() {
+		this.destroyed.next();
+		this.destroyed.complete();
+	}
 }
