78d720c38b762099a58436e7e44cf88186bace10
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-03 15:42:37 -0300

    feat: miscellaneous

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,10 +3,19 @@ import { CommonModule } from '@angular/common';
 import { ProdutsListComponent } from './components/produts-list/produts-list.component';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
 	declarations: [ProdutsListComponent],
-	imports: [CommonModule, FlexLayoutModule],
+	imports: [
+		CommonModule,
+		FlexLayoutModule,
+		MatCardModule,
+		MatButtonModule,
+		MatIconModule,
+	],
 	exports: [ProdutsListComponent],
 })
 export class SharedModule {}
