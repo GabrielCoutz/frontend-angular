1bc2602f482e419ee41a0e2ed22a3e8e455e031d
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-04 11:59:55 -0300

    test(home.component): home component tested and pass

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,22 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { IntroComponent } from '../../modules/home/components/intro/intro.component';
+import { SharedModule } from '../../modules/shared/shared.module';
+import { ProductService } from '../../services/product/product.service';
+import { productExpectPayload } from '../../services/product/product.service.mocks';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
-	let component: HomeComponent;
+	let homeComponent: HomeComponent;
 	let fixture: ComponentFixture<HomeComponent>;
 
 	beforeEach(async () => {
-		await TestBed.configureTestingModule({
-			declarations: [HomeComponent],
-		}).compileComponents();
+		const productServiceSpy = jasmine.createSpyObj<ProductService>({
+			get: of(productExpectPayload),
+			getAll: of([productExpectPayload]),
+			create: of(productExpectPayload),
+			update: of(productExpectPayload),
+			delete: undefined,
+		});
 
+		await TestBed.configureTestingModule({
+			declarations: [HomeComponent, IntroComponent],
+			imports: [SharedModule],
+			providers: [
+				{
+					provide: ProductService,
+					useValue: productServiceSpy,
+				},
+			],
+		}).compileComponents();
 		fixture = TestBed.createComponent(HomeComponent);
-		component = fixture.componentInstance;
+
+		homeComponent = fixture.componentInstance;
 		fixture.detectChanges();
 	});
 
 	it('should create', () => {
-		expect(component).toBeTruthy();
+		expect(homeComponent).toBeTruthy();
 	});
 });
