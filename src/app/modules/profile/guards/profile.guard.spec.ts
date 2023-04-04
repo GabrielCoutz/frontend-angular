44e89696b39f52875295dad4e161b54a15a7016f
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-04 16:01:48 -0300

    test(profile.guard): tests created in profile.guard, but only achieve 40% coverage
    for now, i don't have knowledge enough to make more than 80% ... so, i left my attempt commented

--- src/app/modules/profile/guards/profile.guard.spec.ts
+++ src/app/modules/profile/guards/profile.guard.spec.ts
@@ -1,16 +1,75 @@
+import { HttpClient } from '@angular/common/http';
+import {
+	HttpClientTestingModule,
+	HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import {
+	ActivatedRouteSnapshot,
+	Router,
+	RouterStateSnapshot,
+} from '@angular/router';
+import { AuthService } from '../../../services/auth/auth.service';
+import { authUserTokenExpect } from '../../../services/auth/auth.service.mocks';
+
+import { of, throwError } from 'rxjs';
 
 import { ProfileGuard } from './profile.guard';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ProfileGuard', () => {
+	let httpTestingController: HttpTestingController;
+	let httpClient: HttpClient;
 	let guard: ProfileGuard;
+	let authService: AuthService;
+	let router: Router;
+	const mockActivatedRouteSnapshot =
+		jasmine.createSpyObj<ActivatedRouteSnapshot>('ActivatedRouteSnapshot', [
+			'toString',
+		]);
+	const mockRouterStateSnapshot = jasmine.createSpyObj<RouterStateSnapshot>(
+		'RouterStateSnapshot',
+		['toString']
+	);
 
 	beforeEach(() => {
-		TestBed.configureTestingModule({});
+		TestBed.configureTestingModule({
+			imports: [HttpClientTestingModule, RouterTestingModule],
+		});
+		httpTestingController = TestBed.inject(HttpTestingController);
+		httpClient = TestBed.inject(HttpClient);
+		authService = TestBed.inject(AuthService);
+		router = TestBed.inject(Router);
 		guard = TestBed.inject(ProfileGuard);
 	});
 
 	it('should be created', () => {
 		expect(guard).toBeTruthy();
 	});
+
+	it('should return true when user is logged in', () => {
+		spyOn(authService, 'validate').and.returnValue(of(authUserTokenExpect));
+
+		expect(
+			guard.canActivate(mockActivatedRouteSnapshot, mockRouterStateSnapshot)
+		).toBeTruthy();
+	});
+
+	// O teste abaixo é para verificar se caso o usuário não for encontrado, o guard redireciona
+	// para a página de login.
+
+	// No momento não possuo conhecimento o suficiente para fazer funcionar, mas fica aí minha tentativa.
+
+	// it('should redirect if user is not logged in', () => {
+	// 	spyOn(authService, 'validate').and.returnValue(
+	// 		throwError(() => new Error('User is not logged in'))
+	// 	);
+	// 	spyOn(router, 'navigate');
+
+	// 	guard
+	// 		.canActivate(mockActivatedRouteSnapshot, mockRouterStateSnapshot)
+	// 		.subscribe(() =>
+	// 			expect(router.navigate).toHaveBeenCalledWith(['/account/signin'])
+	// 		);
+	// });
 });
--- src/app/modules/profile/guards/profile.guard.ts
+++ src/app/modules/profile/guards/profile.guard.ts
@@ -5,7 +5,7 @@ import {
 	RouterStateSnapshot,
 	UrlTree,
 } from '@angular/router';
-import { catchError, Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { map } from 'rxjs';
 @Injectable({
@@ -19,25 +19,22 @@ export class ProfileGuard {
 	canActivate(
 		route: ActivatedRouteSnapshot,
 		state: RouterStateSnapshot
-	): boolean | UrlTree | Observable<boolean | UrlTree> {
+	): Observable<boolean | UrlTree> {
 		const token = localStorage.getItem('token');
 		if (!token) {
 			this.router.navigate(['/account/signin']);
-			return false;
+			return of(false);
 		}
 
-		return this.authService
-			.validate(token)
-			.pipe(
-				map((value) => {
-					if (!value) {
-						this.router.navigate(['/account/signin']);
-						return false;
-					}
-
-					return true;
-				})
-			)
-			.pipe(catchError(() => this.router.navigate(['/account/signin'])));
+		return this.authService.validate(token).pipe(
+			map((value) => {
+				if (!value) {
+					this.router.navigate(['/account/signin']);
+					return false;
+				}
+				return true;
+			}),
+			catchError(() => this.router.navigate(['/account/signin']))
+		);
 	}
 }
