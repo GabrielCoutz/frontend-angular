4d4414bc147342477fd63bede83bab8ed53b5bd7
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-04 10:18:53 -0300

    test(auth.service): all tests in auth.service methods complete and pass

---
+++ src/app/services/auth/auth.service.mocks.ts
@@ -0,0 +1,15 @@
+export const authUserPayload = {
+	email: 'user@example.com',
+	password: 'password',
+};
+
+export const authUserResponse = {
+	id: '123',
+	token: '654321',
+};
+
+export const authUserTokenPayload = '654321';
+
+export const authUserTokenExpect = {
+	id: '123456',
+};
--- src/app/services/auth/auth.service.spec.ts
+++ src/app/services/auth/auth.service.spec.ts
@@ -1,16 +1,89 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+	HttpClientTestingModule,
+	HttpTestingController,
+} from '@angular/common/http/testing';
 import { AuthService } from './auth.service';
+import { HttpClient } from '@angular/common/http';
+import {
+	authUserPayload,
+	authUserResponse,
+	authUserTokenExpect,
+	authUserTokenPayload,
+} from './auth.service.mocks';
+
+import { throwError } from 'rxjs';
 
 describe('AuthService', () => {
-	let service: AuthService;
+	let httpTestingController: HttpTestingController;
+	let httpClient: HttpClient;
+	let authService: AuthService;
 
 	beforeEach(() => {
-		TestBed.configureTestingModule({});
-		service = TestBed.inject(AuthService);
+		TestBed.configureTestingModule({
+			imports: [HttpClientTestingModule],
+		});
+
+		httpTestingController = TestBed.inject(HttpTestingController);
+		httpClient = TestBed.inject(HttpClient);
+		authService = TestBed.inject(AuthService);
+	});
+
+	afterEach(() => {
+		httpTestingController.verify();
 	});
 
 	it('should be created', () => {
-		expect(service).toBeTruthy();
+		expect(authService).toBeTruthy();
+	});
+
+	describe('Signin user', () => {
+		it('should signin', () => {
+			authService.signin(authUserPayload).subscribe({
+				next: (response) => expect(response).toEqual(authUserResponse),
+				complete: () => expect(true).toBeTruthy(),
+			});
+
+			const req = httpTestingController.expectOne(
+				'http://localhost:3000/auth/login'
+			);
+			expect(req.request.method).toEqual('POST');
+			req.flush(authUserResponse);
+		});
+
+		it('should return an error with invalid credentials', () => {
+			spyOn(authService, 'signin').and.returnValue(
+				throwError(() => new Error('Invalid credentials'))
+			);
+
+			authService.signin(authUserPayload).subscribe({
+				error: (erro) => expect(erro.message).toEqual('Invalid credentials'),
+			});
+		});
+	});
+
+	describe('Validate credentials', () => {
+		it('should validate and return id from bearer token', () => {
+			authService.validate(authUserTokenPayload).subscribe({
+				next: (response) => expect(response).toEqual(authUserTokenExpect),
+				complete: () => expect(true).toBeTruthy(),
+			});
+
+			const req = httpTestingController.expectOne(
+				'http://localhost:3000/auth/validate'
+			);
+			expect(req.request.method).toEqual('GET');
+			req.flush(authUserTokenExpect);
+		});
+
+		it('should return an error with invalid credentials', () => {
+			spyOn(authService, 'validate').and.returnValue(
+				throwError(() => new Error('Invalid credentials'))
+			);
+
+			authService.validate(authUserTokenPayload).subscribe({
+				error: (erro) => expect(erro.message).toEqual('Invalid credentials'),
+			});
+		});
 	});
 });
