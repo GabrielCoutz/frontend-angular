afd45eb792621cccd1d1bc076543399a3b3296b7
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-06 19:55:35 -0300

    feat(app.module): ngrx added to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -14,6 +14,10 @@ import { MatMenuModule } from '@angular/material/menu';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { userReducer } from './store/user/user.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { productsReducer } from './store/product/product.reducer';
 
 @NgModule({
 	declarations: [AppComponent, HeaderComponent],
@@ -30,6 +34,11 @@ import { HttpClientModule } from '@angular/common/http';
 		MatSidenavModule,
 		AppRoutingModule,
 		HttpClientModule,
+		StoreModule.forRoot({
+			user: userReducer,
+			products: productsReducer,
+		}),
+		StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
 	],
 	providers: [],
 	bootstrap: [AppComponent],
