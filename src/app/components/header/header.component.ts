da596b0ee10dedcb9029fcf9c03e0bc8eacf2895
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-06 20:00:44 -0300

    feat(header.component): header component now have dinamic login button
    now login button consult ngrx store, and if user is logged, change name of user and redirect to user
    profile. If isn't, name is 'login' and redirect to signin page.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,7 +4,10 @@ import {
 	BreakpointState,
 } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Subject, takeUntil } from 'rxjs';
+
+import { selectUser } from '../../store/user/user.selectors';
 
 @Component({
 	selector: 'app-header',
@@ -12,9 +15,13 @@ import { Subject, takeUntil } from 'rxjs';
 	styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-	constructor(private readonly breakpointObserver: BreakpointObserver) {}
+	constructor(
+		private readonly breakpointObserver: BreakpointObserver,
+		private readonly store: Store
+	) {}
 	destroyed = new Subject<void>();
 	mobileView = false;
+	usuario$ = this.store.select(selectUser);
 
 	ngOnInit() {
 		this.breakpointObserver
