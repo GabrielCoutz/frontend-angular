9a7d2ccb97b367186681be9e8148622e8db33627
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-08 15:22:37 -0300

    feat(loading.component): loading component created

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -12,17 +12,25 @@ import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 import { CurrencyBrPipe } from '../../pipes/currency-br.pipe';
 import { ErrorComponent } from './components/error/error.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 registerLocaleData(localePt);
 
 @NgModule({
-	declarations: [ProdutsListComponent, CurrencyBrPipe, ErrorComponent],
+	declarations: [
+		ProdutsListComponent,
+		CurrencyBrPipe,
+		ErrorComponent,
+		LoadingComponent,
+	],
 	imports: [
 		CommonModule,
 		FlexLayoutModule,
 		MatCardModule,
 		MatButtonModule,
 		MatIconModule,
+		MatProgressSpinnerModule,
 	],
 	exports: [ProdutsListComponent, ErrorComponent],
 	providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
