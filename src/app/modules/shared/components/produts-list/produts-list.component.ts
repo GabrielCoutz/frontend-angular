b303a74b774999728deb6c8eff9b1c9b7e479ebe
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-08 15:23:04 -0300

    feat: loading component implemented

--- src/app/modules/shared/components/produts-list/produts-list.component.ts
+++ src/app/modules/shared/components/produts-list/produts-list.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {
-	selectAllProductsState,
-	selectProductErrorState,
+	selectAllProducts,
+	selectProductError,
+	selectProductLoading,
 } from '../../../../store/product/product.selectors';
 
 @Component({
@@ -12,6 +13,7 @@ import {
 })
 export class ProdutsListComponent {
 	constructor(private readonly store: Store) {}
-	products$ = this.store.select(selectAllProductsState);
-	error$ = this.store.select(selectProductErrorState);
+	products$ = this.store.select(selectAllProducts);
+	error$ = this.store.select(selectProductError);
+	loading$ = this.store.select(selectProductLoading);
 }
