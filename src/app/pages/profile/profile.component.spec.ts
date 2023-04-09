ac7c448757f0c9474bb870ab537b3470757dc93a
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-09 20:28:55 -0300

    test(profile.component): changed to use Ngrx instead UserService. 100% coverage

--- src/app/pages/profile/profile.component.spec.ts
+++ src/app/pages/profile/profile.component.spec.ts
@@ -1,67 +1,43 @@
-import { HttpClient } from '@angular/common/http';
-import {
-	HttpClientTestingModule,
-	HttpTestingController,
-} from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { of } from 'rxjs';
 import { ProfileModule } from '../../modules/profile/profile.module';
-import { UserService } from '../../services/user/user.service';
-import { userExpectPayload } from '../../services/user/user.service.mocks';
 
 import { ProfileComponent } from './profile.component';
 
 import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 describe('ProfileComponent', () => {
-	let httpTestingController: HttpTestingController;
-	let component: ProfileComponent;
-	let httpClient: HttpClient;
+	let profileComponent: ProfileComponent;
 	let fixture: ComponentFixture<ProfileComponent>;
 	let store: MockStore;
 
 	beforeEach(async () => {
-		const userServiceSpy = jasmine.createSpyObj<UserService>({
-			get: of(userExpectPayload),
-			getAll: of([userExpectPayload]),
-			create: of(userExpectPayload),
-			update: of(userExpectPayload),
-			delete: undefined,
-		});
-
 		await TestBed.configureTestingModule({
 			declarations: [ProfileComponent],
-			imports: [
-				ProfileModule,
-				HttpClientTestingModule,
-				RouterTestingModule,
-				BrowserAnimationsModule,
-			],
-			providers: [
-				{
-					provide: UserService,
-					useValue: userServiceSpy,
-				},
-				provideMockStore({}),
-			],
+			imports: [ProfileModule, RouterTestingModule, BrowserAnimationsModule],
+			providers: [provideMockStore({})],
 		}).compileComponents();
 
-		httpTestingController = TestBed.inject(HttpTestingController);
 		fixture = TestBed.createComponent(ProfileComponent);
-		httpClient = TestBed.inject(HttpClient);
+
 		store = TestBed.inject(MockStore);
 
-		component = fixture.componentInstance;
+		profileComponent = fixture.componentInstance;
 		fixture.detectChanges();
 	});
 
-	afterEach(() => {
-		httpTestingController.verify();
+	it('should create', () => {
+		expect(profileComponent).toBeTruthy();
 	});
 
-	it('should create', () => {
-		expect(component).toBeTruthy();
+	describe('On init', () => {
+		it('should dispatch loadCurrentUser', () => {
+			spyOn(store, 'dispatch').and.callThrough();
+
+			profileComponent.ngOnInit();
+
+			expect(store.dispatch).toHaveBeenCalled();
+		});
 	});
 });
