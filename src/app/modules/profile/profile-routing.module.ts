a4cde1fa49dcd3477c83884ec50c72ca837f60aa
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-10 23:37:20 -0300

    feat(profile-routing.module): children routes created

--- src/app/modules/profile/profile-routing.module.ts
+++ src/app/modules/profile/profile-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from 'src/app/pages/profile/profile.component';
+import { DangerZoneComponent } from './components/danger-zone/danger-zone.component';
+import { MyProductsComponent } from './components/my-products/my-products.component';
+import { ProfileFormComponent } from './components/profile-form/profile-form.component';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: ProfileComponent,
+		children: [
+			{
+				path: 'edit',
+				component: ProfileFormComponent,
+			},
+			{
+				path: 'products',
+				component: MyProductsComponent,
+			},
+			{
+				path: 'config',
+				component: DangerZoneComponent,
+			},
+		],
 	},
 ];
 
