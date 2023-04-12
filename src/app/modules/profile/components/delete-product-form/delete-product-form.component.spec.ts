a90579538f77d8b9f82640a694218c14d35838f6
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-12 19:18:59 -0300

    style: run lint

--- src/app/modules/profile/components/delete-product-form/delete-product-form.component.spec.ts
+++ src/app/modules/profile/components/delete-product-form/delete-product-form.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DeleteProductFormComponent } from './delete-product-form.component';
 
 describe('DeleteProductFormComponent', () => {
-  let component: DeleteProductFormComponent;
-  let fixture: ComponentFixture<DeleteProductFormComponent>;
+	let component: DeleteProductFormComponent;
+	let fixture: ComponentFixture<DeleteProductFormComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DeleteProductFormComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [DeleteProductFormComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(DeleteProductFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(DeleteProductFormComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- src/app/modules/profile/components/my-products/my-products.component.spec.ts
+++ src/app/modules/profile/components/my-products/my-products.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MyProductsComponent } from './my-products.component';
 
 describe('MyProductsComponent', () => {
-  let component: MyProductsComponent;
-  let fixture: ComponentFixture<MyProductsComponent>;
+	let component: MyProductsComponent;
+	let fixture: ComponentFixture<MyProductsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MyProductsComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [MyProductsComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(MyProductsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(MyProductsComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
