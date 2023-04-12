9ff71c73a7982a8b1b453ad2d4ef8521909783a3
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-11 23:49:07 -0300

    feat(my-products.component): component to load current user products complete

--- src/app/modules/profile/components/my-products/my-products.component.ts
+++ src/app/modules/profile/components/my-products/my-products.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectCurrentUserProducts } from '../../../../store/currentUser/currentUser.selectors';
 
 @Component({
-  selector: 'app-my-products',
-  templateUrl: './my-products.component.html',
-  styleUrls: ['./my-products.component.scss']
+	selector: 'app-my-products',
+	templateUrl: './my-products.component.html',
+	styleUrls: ['./my-products.component.scss'],
 })
 export class MyProductsComponent {
+	constructor(private readonly store: Store) {}
 
+	products$ = this.store.select(selectCurrentUserProducts);
 }
