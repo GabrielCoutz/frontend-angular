a4cdc5029ceffde098a8eb68614ed7f6f7e90222
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-04-14 16:19:52 -0300

    feat(signin.component): signin page auto redirect to profile if user is logged

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { selectCurrentUserId } from '../../store/currentUser/currentUser.selectors';
 
 @Component({
 	selector: 'app-signin',
 	templateUrl: './signin.component.html',
 	styleUrls: ['./signin.component.scss'],
 })
-export class SigninComponent {}
+export class SigninComponent {
+	constructor(private readonly store: Store, private readonly router: Router) {}
+	userId$ = this.store.select(selectCurrentUserId);
+
+	ngOnInit() {
+		this.userId$.subscribe((id) => {
+			if (id) this.router.navigate(['/profile', id, 'edit']);
+		});
+	}
+}
